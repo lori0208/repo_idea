9a7d1cd34b3813e022502cac79f3c1b41f959951
lori0208 <1577701718@qq.com>
2022-01-11 16:58:20 +0800

    Update GitMapper.java

--- lagou_edu_home_parent/ssm-dao/src/main/java/com/zxz/dao/GitMapper.java
+++ lagou_edu_home_parent/ssm-dao/src/main/java/com/zxz/dao/GitMapper.java
@@ -8,4 +8,14 @@ package com.zxz.dao;
 public interface GitMapper {
 
     public void add();
+    
+    
+    public void add1();
+    public void add2();
+    public void add3();
+    public void add4();
+    public void add5();
+    
+    
+    
 }
